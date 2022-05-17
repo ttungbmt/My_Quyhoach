a7ef62a52d240d519c0ba13d06be90e1040d5ab1
Trương Thanh Tùng <ttungbmt@gmail.com>
2022-05-17 13:39:09 +0700

    Update web.php

--- laravel-app/routes/web.php
+++ laravel-app/routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/test', function () {
+    $model = \App\Models\User::first();
+    dd($model);
+    return view('welcome');
+});
