0bc42423d47828b422579c8f3e17c57fb19850d2
$ <$>
2022-06-04 13:38:16 +0700

    Update changes

--- laravel-app/app/Http/Controllers/API/ThuadatController.php
+++ laravel-app/app/Http/Controllers/API/ThuadatController.php
@@ -7,18 +7,38 @@ use Illuminate\Http\Request;
 
 class ThuadatController extends Controller
 {
+    public function view($id){
+        $model = Thuadat::findOrFail($id);
+
+        return $this->formatThuadat($model);
+    }
+
+    protected function formatThuadat($model){
+        return [
+            'id' => $model->id,
+            'tinh_tp' => 'Lâm Đồng',
+            'quanhuyen' => 'Đức Trọng',
+            'phuongxa' => 'Đà Loan',
+            'sothua' => $model->shthua,
+            'soto' => $model->shbando,
+            'dientich' => $model->dientich,
+            'geometry' => $model->geom
+        ];
+    }
+
     public function getByInfo(Request $request){
         $model = Thuadat::where('shbando', $request->input('soto'))
             ->where('shthua', $request->input('sothua'))
             ->first();
 
-        return $model;
+        return $this->formatThuadat($model);
     }
 
     public function getByLocation(Request $request){
         $location = $request->input('location');
         $model = Thuadat::whereRaw("ST_Intersects('SRID=4326;POINT({$location['lat']} {$location['lng']})', geom)")->get();
-        return $model;
+        if($model) return $this->formatThuadat($model);
+        return null;
     }
 
     public function getByCoords(Request $request){
@@ -29,6 +49,6 @@ class ThuadatController extends Controller
             $model = $model->whereRaw("ST_Intersects(ST_Transform(ST_GeomFromText('POINT({$coord['y']} {$coord['x']})', 3405), 4326), geom)");
         }
 
-        return $model->first();
+        return $this->formatThuadat($model->first());
     }
 }
